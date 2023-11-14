2fef6dbd28620ced90394cf726aea9f2a3566302
Blaž Škufca <3877198+blazskufca@users.noreply.github.com>
2023-11-14 18:27:52 +0100

    minor improvements

--- run_spider_wrapper.py
+++ run_spider_wrapper.py
@@ -1,3 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import signal
 from os import environ
 from io import StringIO
 from requests import post
@@ -8,8 +12,7 @@ from threading import Thread, Lock
 from subprocess import CalledProcessError, Popen, PIPE
 from re import Match, Pattern, DOTALL, search, compile
 from argparse import ArgumentParser, REMAINDER, Namespace
-from typing import Optional, TextIO, List, Iterable, IO, AnyStr, Dict
-
+from typing import Optional, TextIO, List, Iterable, IO, AnyStr, Dict, Any
 
 NTFY_INSTANCE_ADDRESS: str = environ.get("ntfy_instance_address", "ntfy.sh")
 NTFY_TOPIC: str = environ.get("ntfy_topic", "spiders")
@@ -46,16 +49,18 @@ class Tee(StringIO):
         """Provide a fileno() method like a real file object."""
         return stdout.fileno()
 
-
-def capture_output(source: IO, tee_instance: Tee) -> None:
-    for line in iter(source.readline, ''):
-        tee_instance.write(line)
+    def capture_output(self, source: Optional[IO[AnyStr]], *args: Any, **kwargs: Any) -> None:
+        for line in iter(source.readline, ''):
+            self.write(line)
 
 
 def run_spider(spider_name: str, spider_args: Optional[Iterable[str]] = None) -> Optional[str]:
+    process: Optional[Popen] = None
+    tee: Optional[Tee] = None
     try:
         # Construct the command to run the Scrapy spider
-        command: List[str] = ['scrapy', 'crawl', spider_name]
+        # Set log level to info, because that is more than enough to get dumped stats + smaller str buffer
+        command: List[str] = ['scrapy', 'crawl', spider_name, "-L", "INFO"]
 
         # Add any additional arguments for the spider
         if spider_args:
@@ -72,22 +77,34 @@ def run_spider(spider_name: str, spider_args: Optional[Iterable[str]] = None) ->
                 universal_newlines=True
         ) as process:
             # Create threads for capturing stdout and stderr
-            stdout_thread: Thread = Thread(target=capture_output, args=(process.stdout, tee))
-            stderr_thread: Thread = Thread(target=capture_output, args=(process.stderr, tee))
+            stdout_thread: Thread = Thread(target=tee.capture_output, args=(process.stdout, ))
+            stderr_thread: Thread = Thread(target=tee.capture_output, args=(process.stderr, ))
             stdout_thread.start()
             stderr_thread.start()
             process.wait()
             stdout_thread.join()
             stderr_thread.join()
-        return tee.close()
+
     except CalledProcessError as e:
-        return f"Error running Scrapy spider: {e}"
+        print(f"{RED_COLOR}ERROR: {e}{END_COLOR}")
+    finally:
+        if process and hasattr(process, 'pid'):
+            # Kill the Scrapy spider if it's still running
+            print(f"{RED_COLOR}Process {process.pid} is still running although it shouldn't, sending it"
+                  f" SIGINT{END_COLOR}")
+            process.send_signal(signal.SIGINT)
+            process.wait()
+        if tee and isinstance(tee, Tee):
+            return tee.close()
+        else:
+            return "Error running Scrapy spider!"
 
 
-def postprocess_scrapy_log(log: str) -> AnyStr:
+def postprocess_scrapy_log(log: Optional[str] = None) -> AnyStr:
     pattern: Pattern[AnyStr] = compile(r'INFO: Dumping Scrapy stats:.*', DOTALL)
-    match: Optional[Match] = search(pattern, log)
-
+    match: Optional[Match] = None
+    if log is not None:
+        match = search(pattern, log)
     if match:
         extracted_info: AnyStr = match.group(0)
         return extracted_info
@@ -98,7 +115,6 @@ def postprocess_scrapy_log(log: str) -> AnyStr:
 def send_request(spider_name: str, output_arg: AnyStr, instance_address: str = NTFY_INSTANCE_ADDRESS,
                  topic: str = NTFY_TOPIC, username: Optional[str] = NTFY_USERNAME,
                  password: Optional[str] = NTFY_PASSWORD, bearer: str = NTFY_BEARER) -> Optional[None]:
-
     url: str = f"https://{instance_address}/{topic}"
     headers: Dict[str] = dict(title=f"Scrapy spider {spider_name} finished!")
 
@@ -110,14 +126,15 @@ def send_request(spider_name: str, output_arg: AnyStr, instance_address: str = N
     if isinstance(output_arg, bytes):
         output_arg = output_arg.decode('utf-8')
     if (byte_size := getsizeof(output_arg)) >= 4096:
-        print(f"{RED_COLOR}WARNING: You'll receive you're log as an attachment, because it's {byte_size} bytes long."
+        headers["filename"] = f"{spider_name}.log"
+        print(f"{RED_COLOR}WARNING: You'll receive your log as an attachment, because it's {byte_size} bytes long."
               f"{END_COLOR}")
 
     data: str = f"Finished at {datetime.now()} Output: {output_arg}"
 
     post(
         url,
-        data=data,
+        data=data.encode('utf-8'),
         headers=headers
     ).raise_for_status()
 
